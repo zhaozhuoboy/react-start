33aff6b12ec2c1d7f46c92b7613266f78ffee2cf
DevZhao <zhaozhuoboy@163.com>
2018-04-23 17:52:01 +0800

    add axios && use async function to fetch github

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -5,20 +5,32 @@
 */
 
 import React from 'react';
-
+import axios from 'axios';
 
 export default class About extends React.Component {
     constructor(props) {
         super(props);
+        this.state={
+            username:""
+        }
     }
     componentDidMount() {
 
+    }
+    fetch = async () => {
+        const res = await axios.get('https://api.github.com/users/zhaozhuoboy');
+        console.log(res)
+        this.setState({
+            username:res.data.name
+        })
     }
     render() {
         console.log(this.props)
         return (
             <div className="about">
-                About page
+                <h2 style={{textAlign:'center'}}>About page</h2>
+                <button style={{ textAlign: 'center' }} onClick={this.fetch}>Fetch info</button>
+                <h3 style={{color:"green"}}>{this.state.username}</h3>
             </div>
         );
     }
