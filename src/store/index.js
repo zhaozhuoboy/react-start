e3e05731629060e4cd155ae476ab5f91903b82a0
DevZhao <zhaozhuoboy@163.com>
2018-05-30 08:46:29 +0800

    chrom redux devtool

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore,applyMiddleware} from 'redux';
 import ReduxThunk from 'redux-thunk';
 import rootReducer from '../reducer';
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const store = createStore(rootReducer,/* preloadedState, */
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        applyMiddleware(ReduxThunk));
 
 export default store;
